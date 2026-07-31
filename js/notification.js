57827e359be417b6d3e67ff5af3c8415b143e95f
Miyaz U <miyazumare@gmail.com>
2026-07-31 11:08:19 +0530

    Refactor notification functions for appointments and contacts

--- js/notification.js
+++ js/notification.js
@@ -1,8 +1,22 @@
 const notification = document.getElementById("notification")
 
-function showNotification(message) {
+function showNotificationAppointment(message) {
     notification.innerHTML = `
         <strong>✔️ Appointment Booked Successfully</strong>
+        <p>${message}
+    `
+    notification.classList.remove("hidden")
+    notification.classList.add("show")
+
+    setTimeout(function () {
+        notification.classList.remove("show")
+        notification.classList.add("hidden")
+    }, 3000)
+}
+
+function showNotificationContact(message) {
+    notification.innerHTML = `
+        <strong>✔️ Message Sent Successfully</strong>
         <p>${message}
     `
     notification.classList.remove("hidden")
